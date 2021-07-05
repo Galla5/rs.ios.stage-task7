35ca0b99d03ec43caec9ba1424dc8c77834a8645
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-07-05 19:15:20 +0300

    feat: add ios project

---
+++ LoginTask/LoginTask/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LoginTask
+//
+//  Created by Macbook on 5.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LoginTask/LoginTask/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LoginTask
+//
+//  Created by Macbook on 5.07.21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
