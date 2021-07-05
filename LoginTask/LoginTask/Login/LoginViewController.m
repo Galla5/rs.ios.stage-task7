222c2bfa445d11601a0f1d4329036c819aa9fc4a
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-07-05 20:31:43 +0300

    update authorize logic

--- LoginTask/LoginTask/Login/LoginViewController.m
+++ LoginTask/LoginTask/Login/LoginViewController.m
@@ -8,6 +8,9 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *loginTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *authorizeButton;
 
 @end
 
@@ -15,17 +18,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self setupTextFields];
+    [self setupButton];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setupTextFields {
+    self.loginTextField.layer.cornerRadius = 5;
+    self.loginTextField.layer.borderWidth = 1.5;
+    self.loginTextField.layer.borderColor = [UIColor blackColor].CGColor;
+    self.passwordTextField.layer.cornerRadius = 5;
+    self.passwordTextField.layer.borderWidth = 1.5;
+    self.passwordTextField.layer.borderColor = [UIColor blackColor].CGColor;
 }
-*/
+
+- (void)setupButton {
+    // Setup
+    self.authorizeButton.layer.borderWidth = 2.0;
+    self.authorizeButton.layer.cornerRadius = 10.0;
+    [self.authorizeButton.layer setBorderColor: [UIColor colorWithRed: 0.50 green: 0.64 blue: 0.93 alpha: 1.00].CGColor];
+    [self.authorizeButton setImage:[UIImage systemImageNamed:@"person"] forState:normal];
+    
+    // Insets
+    self.authorizeButton.contentEdgeInsets = UIEdgeInsetsMake(10, 20, 10, 20);
+    self.authorizeButton.imageEdgeInsets = UIEdgeInsetsMake(0, -10, 0, 0);
+    
+    // UIControlStateHighlighted
+    [self.authorizeButton setImage:[UIImage imageNamed:@"person.fill"]forState: UIControlStateHighlighted];
+    [self.authorizeButton setTitleColor:[[UIColor colorWithRed: 0.50 green: 0.64 blue: 0.93 alpha: 1.00] colorWithAlphaComponent:0.4] forState: UIControlStateHighlighted];
+    
+    // UIControlStateDisabled
+    [self.authorizeButton setTitleColor:[[UIColor colorWithRed: 0.50 green: 0.64 blue: 0.93 alpha: 1.00] colorWithAlphaComponent: 0.5] forState: UIControlStateDisabled];
+}
+
+
+- (IBAction)authorizeButtonDidTap:(id)sender {
+    if([self.loginTextField.text isEqual: @"username"]) {
+        self.loginTextField.layer.borderColor = [UIColor colorWithRed: 0.57 green: 0.78 blue: 0.69 alpha: 1.00].CGColor;
+        self.loginTextField.alpha = 0.5;
+        [self.loginTextField setEnabled:NO];
+    } else {
+        self.loginTextField.layer.borderColor = [UIColor colorWithRed: 0.76 green: 0.00 blue: 0.08 alpha: 1.00].CGColor;
+    }
+    
+    if([self.passwordTextField.text isEqual: @"password"]) {
+        self.passwordTextField.layer.borderColor = [UIColor colorWithRed: 0.57 green: 0.78 blue: 0.69 alpha: 1.00].CGColor;
+        self.passwordTextField.alpha = 0.5;
+        [self.passwordTextField setEnabled:NO];
+    } else {
+        self.passwordTextField.layer.borderColor = [UIColor colorWithRed: 0.76 green: 0.00 blue: 0.08 alpha: 1.00].CGColor;
+    }
+    
+    if([self.loginTextField.text isEqual: @"username"] && [self.passwordTextField.text isEqual: @"password"]) {
+        self.authorizeButton.layer.opacity = 0.5;
+        [self.authorizeButton setUserInteractionEnabled:NO];
+    }
+}
 
 @end
