b3025d0ef838169604981d27b88e9a1c7830874c
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-07-05 19:44:02 +0300

    Add login vc

--- LoginTask/LoginTask/SceneDelegate.m
+++ LoginTask/LoginTask/SceneDelegate.m
@@ -18,6 +18,14 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    UIWindow *window = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
+    window.backgroundColor = [UIColor whiteColor];
+    UIViewController *controller = [[UIViewController alloc] initWithNibName:@"LoginViewController" bundle:nil];
+//    window = window;
+    window.rootViewController = controller;
+    self.window = window;
+    
+    [self.window makeKeyAndVisible];
 }
 
 
